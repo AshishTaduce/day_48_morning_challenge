018875b7274058497206df3ebe8d76ce6f92ec87
AshishTaduce <ashishteddy2@gmail.com>
2019-12-10 11:12:33 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -19,5 +19,26 @@ class Student{
 
 
 void main() {
+  //Challenge #1
+  List<Student> students = [];
+  students.add(Student('Ram', 87).addStudent());
+  students.add(Student('Shyam', 80).addStudent());
+  students.add(Student('GhanShyamRam', 81).addStudent());
 
+  //Challenge #4
+  students.sort((a, b) => a.score.compareTo(b .score));
+
+  Map<String, int> topStudent = {students[0].name : students[0].score};
+  for (int i = 0; i < students.length; i++){
+    Student student = students[i];
+    Map temp = {student.name : student.score};
+    print(temp);
+    if(topStudent.values.first < student.score){
+      topStudent = {student.name : student.score};
+    }
+  }
+  //Challenge 3
+  print('Topper is ${topStudent.keys} with score of ${topStudent.values.first}');
+students.sort((a, b) => a.score.compareTo(b .score));
+students.reduce(max(a, b) ))
 }
